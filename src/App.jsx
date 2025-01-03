fba57ccaf08383833dac6463f1dbb3bce1519a0a
Lukas Jaiczay <lukas.jaiczay@stud.fh-campuswien.ac.at>
2025-01-03 17:37:37 +0100

    added sidebar to the ui

--- src/App.jsx
+++ src/App.jsx
@@ -4,17 +4,27 @@ import Navbar from './components/Navbar';
 import BearSection from './components/BearSection';
 import Comments from './components/Comments';
 import Footer from './components/Footer';
+import Sidebar from './components/Sidebar';
 
 const App = () => {
     return (
-        <div>
-            <Header />
-            <Navbar />
-            <main>
-                <BearSection />
-                <Comments />
-            </main>
-            <Footer />
+        <div className="layout">
+            <header>
+                <Header />
+            </header>
+            <nav>
+                <Navbar />
+            </nav>
+            <div className="content">
+                <main className="main-content">
+                    <BearSection />
+                    <Comments />
+                </main>
+                <Sidebar />
+            </div>
+            <footer>
+                <Footer />
+            </footer>
         </div>
     );
 };
---
+++ src/components/Sidebar.jsx
@@ -0,0 +1,18 @@
+import React from 'react';
+
+const Sidebar = () => {
+    return (
+        <aside className="sidebar">
+            <h3>Related</h3>
+            <ul>
+                <li><a href="#">The trouble with Bees</a></li>
+                <li><a href="#">The trouble with Otters</a></li>
+                <li><a href="#">The trouble with Penguins</a></li>
+                <li><a href="#">The trouble with Octopi</a></li>
+                <li><a href="#">The trouble with Lemurs</a></li>
+            </ul>
+        </aside>
+    );
+};
+
+export default Sidebar;
