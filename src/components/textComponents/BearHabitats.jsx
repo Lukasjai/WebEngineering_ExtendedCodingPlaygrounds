b2b79310175a1f475f8e88368d484c3535344e8a
Lukas Jaiczay <lukas.jaiczay@stud.fh-campuswien.ac.at>
2025-01-04 20:39:21 +0100

    added the text

---
+++ src/components/textComponents/BearHabitats.jsx
@@ -0,0 +1,33 @@
+
+
+const BearHabitats = () => (
+    <div className="bear-habitats">
+        <h2>Habitats and Eating Habits</h2>
+        <p>
+            Wild bears eat a variety of meat, fish, fruit, nuts, and other naturally growing ingredients. In general,
+            they will hunt for food themselves in woodland or rivers, but at a push they will track down their
+            sustenance
+            from farms or country houses. They tend to live in relative isolation, in caves, tents, or cottages.
+        </p>
+        <div className="image-container">
+            <img src="media/urban-bear.jpg" alt="Urban Bear" className="centered-image"/>
+        </div>
+        <p>
+            Urban (gentrified) bears, on the other hand, have largely abandoned the old ways. They will hunt other
+            urban creatures if necessary (including other predators like rats and foxes), but prefer to scavenge from
+            readily available urban food outlets like dumpsters, bins, and fast food joints. When food has proven
+            scarce,
+            urban bears have even been known to break into people's kitchens and steal essentials like baked beans,
+            ready meals, and microwave ovens.
+        </p>
+        <div className="image-container">
+            <img src="media/wild-bear.jpg" alt="Wild Bear" className="centered-image"/>
+        </div>
+        <p>
+            Urban bears will sleep anywhere they can, from bus shelters and parks to the toilets in McDonalds, to
+            their own apartment.
+        </p>
+    </div>
+);
+
+export default BearHabitats;
